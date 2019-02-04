a69fec5b363b20c20e037d1213d3db61b4d83c9a
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-02-04 15:21:18 +0000

    Finish email server and password reset

--- Surffee/settings.py
+++ Surffee/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'nested_admin',
     'crispy_forms',
+    'surveys.apps.SurveysConfig', # do not move the config pls
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'surveys.apps.SurveysConfig',
 ]
 
 MIDDLEWARE = [
