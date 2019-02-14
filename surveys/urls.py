7d5e73d423e618aa7fa7c280d1caca91a67b4954
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-02-14 17:13:11 +0000

    Create add question page

--- surveys/urls.py
+++ surveys/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:survey_id>/results/', views.results, name="results"),
     path('<int:survey_id>/', views.detail, name="detail"),
+    path('<int:survey_id>/add_question/', views.CreateQuestion.as_view(), name='add_question'),
     path('old_create/', views.CreateSurvey.as_view(), name='old_create'),
     path('create/', views.CreateNewSurvey.as_view(), name='create'),
     path('active/', views.active, name='active'),
--- surveys/views.py
+++ surveys/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 
 from .models import Survey, Question
 from .forms.users import ResearcherCreationForm
-from .forms.models import ResearcherCreateSurvey, QuestionFormSet, ChoiceFormSet
+from .forms.models import ResearcherCreateSurvey, ResearcherCreateQuestion, QuestionFormSet, ChoiceFormSet
 
 
 def index(request):
@@ -81,6 +81,8 @@ class AddQuestion(CreateView):
         form.survey = request.survey_id
         return self.render_to_response(self.get_context_data(form=form))
 """
+
+
 class CreateNewSurvey(CreateView):
     template = 'surveys/create.html'
     model = Survey
@@ -169,5 +171,43 @@ class CreateSurvey(CreateView):
         return self.render_to_response(
             self.get_context_data(form=form,
                                   question_form=question_form,
+                                  choice_form=choice_form)
+        )
+
+
+class CreateQuestion(CreateView):
+    template_name = 'surveys/add_question.html'
+    model = Question
+    form_class = ResearcherCreateQuestion
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        choice_form = ChoiceFormSet
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  choice_form=choice_form)
+        )
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        choice_form = ChoiceFormSet(self.request.POST)
+        if form.is_valid() and choice_form.is_valid():
+            return self.form_valid(form, choice_form)
+        else:
+            return self.form_invalid(form, choice_form)
+
+    def form_valid(self, form, choice_form):
+        self.object = form.save()
+        choice_form.instance = self.object
+        choice_form.save()
+        return redirect('../question/' + str(self.object.id))
+
+    def form_invalid(self, form, choice_form):
+        return self.render_to_response(
+            self.get_context_data(form=form,
                                   choice_form=choice_form)
         )
