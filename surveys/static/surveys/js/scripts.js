f97b9ef702140b16602f03d10d6af4ebd1670d6b
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-02-15 12:39:15 +0000

    Make JS not 🗑️🗑️🗑️

--- surveys/static/surveys/js/scripts.js
+++ surveys/static/surveys/js/scripts.js
@@ -5,21 +5,29 @@ $(document).ready(function () {
     var textBoxCounter = 1;
 
     $("#add-new-choice").click(function () {
-        var oldTextBox = $("#id_choice_set-"+(textBoxCounter-1)+"-choice_text");
+        var oldTextBox = $("#id_choice_set-" + (textBoxCounter-1) + "-choice_text");
         var newTextBox = oldTextBox.clone();
-        newTextBox.attr("name","choice_set-"+textBoxCounter + "-choice_text");
-        newTextBox.attr("id","id_choice_set-"+textBoxCounter + "-choice_text");
-        var newRowDiv = "<div class='row' id='row_div_"+textBoxCounter+"'></div>";
-        $("#answer-bois").append(newRowDiv);
 
-        $("#row_div_"+textBoxCounter).append(newTextBox);
+        newTextBox.attr("name",     "choice_set-"   + textBoxCounter + "-choice_text");
+        newTextBox.attr("id",       "id_choice_set-"+ textBoxCounter + "-choice_text");
+
+        var newRowDiv = $("<div>").addClass('row').attr('id', "row_div_"+textBoxCounter).append(
+            $("<div>").addClass('col-sm-2').text("Choice "+(textBoxCounter+1)),
+            $("<div>").addClass('col-sm-6 form-group').attr('id', "col_div_"+textBoxCounter)
+        );
+
+        $("#answer-bois").append(newRowDiv);
+        $("#col_div_"+textBoxCounter).append(newTextBox);
         $("#id_choice_set-TOTAL_FORMS").val(++textBoxCounter);
     });
 
     $("#delete-choice").click(function () {
-       textBoxCounter--;
-       $("#row_div_"+textBoxCounter).remove();
-       $("#id_choice_set-TOTAL_FORMS").val(textBoxCounter);
+        if (textBoxCounter>1) {
+            textBoxCounter--;
+        }
+        $("#col_div_"+textBoxCounter).remove();
+        $("#row_div_"+textBoxCounter).remove();
+        $("#id_choice_set-TOTAL_FORMS").val(textBoxCounter);
     });
 
     function redirect() {
