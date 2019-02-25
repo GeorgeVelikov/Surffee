46b9b4f1f4d0c97fc3dd697d0827ca65637125ba
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-02-25 10:38:01 +0000

    Show what PI questions will be in a survey

--- surveys/views/page.py
+++ surveys/views/page.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django.core.exceptions import PermissionDenied
+from ast import literal_eval
 
 from ..models import Survey
 from ..forms.users import ResearcherCreationForm
@@ -46,10 +47,16 @@ def detail(request, survey_id):
         raise PermissionDenied("User is not logged in")
     template = 'surveys/detail.html'
     survey = Survey.objects.get(pk=survey_id)
+
+    # this converts the string representation of a list back to a list
+    choices = literal_eval(survey.pi_choices)
+    for i in range(len(choices)):
+        choices[i] = choices[i].replace("_", " ").capitalize()
+
     if survey.creator != request.user and not request.user.is_superuser:
         raise PermissionDenied("You have tried to access " + survey.name + ". To gain permissions please contact "
                                + survey.creator.email + ".")
-    context = {'survey': survey}
+    context = {'survey': survey, 'choices': choices}
     return render(request, template, context)
 
 
