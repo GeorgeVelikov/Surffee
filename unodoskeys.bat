f5e87c4217c2d46e92aaa07f67f400c5f7fcd017
BananaBoiii <m.woznicki.16@aberdeen.ac.uk>
2019-03-02 17:18:54 +0000

    added test shortcut to unodoskeys.bat

--- unodoskeys.bat
+++ unodoskeys.bat
@@ -3,3 +3,4 @@ doskey make=                py manage.py makemigrations
 doskey migrate=             py manage.py migrate
 doskey createsuperuser=     py manage.py createsuperuser
 doskey shell=               py manage.py shell
+doskey test=                py manage.py test surveys
