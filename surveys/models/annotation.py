77fd1c18a60517d16155c9b3fc2164b3cf92f53e
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-03-02 17:33:12 +0000

    Add model attr to Annotation

--- surveys/models/annotation.py
+++ surveys/models/annotation.py
@@ -5,4 +5,5 @@ from .survey import Choice
 class Annotation(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
     word = models.CharField(max_length=2**8)
+    classifiction = models.CharField(max_length=2**6)
     color = models.CharField(max_length=2*3)  # this is in the example form of #FF00FF
