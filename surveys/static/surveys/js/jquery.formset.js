f5d304b1ede402657df8e9fdaff903c5f78471b6
yiannis <ikeravnos@gmail.com>
2019-03-03 16:29:50 +0000

    FIxing home page 101
    side bar now toggles. page still not working as intended

--- surveys/static/surveys/js/jquery.formset.js
+++ surveys/static/surveys/js/jquery.formset.js
@@ -234,26 +234,37 @@
 })(jQuery);
 
 jQuery(function ($) {
+
     $(".sidebar-dropdown > a").click(function() {
-    $(".sidebar-submenu").slideUp(200);
-    if (
-        $(this)
-        .parent()
-        .hasClass("active")
-    ) {
+  $(".sidebar-submenu").slideUp(200);
+  if (
+    $(this)
+      .parent()
+      .hasClass("active")
+  ) {
     $(".sidebar-dropdown").removeClass("active");
     $(this)
       .parent()
       .removeClass("active");
-    } else {
-        $(".sidebar-dropdown").removeClass("active");
-        $(this)
-          .next(".sidebar-submenu")
-          .slideDown(200);
-        $(this)
-          .parent()
-          .addClass("active");
-    }
-    });
+  } else {
+    $(".sidebar-dropdown").removeClass("active");
+    $(this)
+      .next(".sidebar-submenu")
+      .slideDown(200);
+    $(this)
+      .parent()
+      .addClass("active");
+  }
+});
 
-    });
+$("#close-sidebar").click(function() {
+  $(".page-wrapper").removeClass("toggled");
+});
+$("#show-sidebar").click(function() {
+  $(".page-wrapper").addClass("toggled");
+});
+
+
+
+
+});
