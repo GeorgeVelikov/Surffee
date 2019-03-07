9648b0390a46eb7905e5290804a8741241a34dee
BananaBoiii <m.woznicki.16@aberdeen.ac.uk>
2019-03-07 21:44:49 +0000

    just switched places of two if statements for more aesthetic code

--- surveys/views/site.py
+++ surveys/views/site.py
@@ -36,12 +36,12 @@ def index(request):
     context = {}
     if not request.user.is_authenticated:  # user is not logged in
         raise PermissionDenied("User is not logged in")
-    if request.user.is_superuser:
-        context['all_active_surveys'] = Survey.objects.filter(active=True)
-        context['all_inactive_surveys'] = Survey.objects.filter(active=False)
     if request.user.is_authenticated:
         context['my_active_surveys'] = Survey.objects.filter(creator=request.user, active=True)
         context['my_inactive_surveys'] = Survey.objects.filter(creator=request.user, active=False)
+    if request.user.is_superuser:
+        context['all_active_surveys'] = Survey.objects.filter(active=True)
+        context['all_inactive_surveys'] = Survey.objects.filter(active=False)
     return render(request, template, context)
 
 
