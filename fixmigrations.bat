ede8d3463aa25297ef72bb4670a363a8e49b8383
Ninrich <nino.trnovsky@gmail.com>
2019-03-07 17:05:23 +0000

    improve fixmigrations, var scopes in .js file

--- fixmigrations.bat
+++ fixmigrations.bat
@@ -1,1 +1,1 @@
-del /q "surveys\migrations\0*" && del db.sqlite3 && python manage.py makemigrations && python manage.py migrate
+del /q "surveys\migrations\0*" && del db.sqlite3 && python manage.py makemigrations && python manage.py migrate && python manage.py createsuperuser
--- surveys/static/surveys/js/scripts.js
+++ surveys/static/surveys/js/scripts.js
@@ -3,24 +3,24 @@ $(document).ready(function () {
     feather.replace();
 
     // global tb counter
-    var textBoxCounter = 1;
+    let textBoxCounter = 1;
 
 
     // check if the element exists first
     if ($("#add-new-choice").length) {
         // shows all choices in a question as the page loads
-        $("#add-new-choice").ready(function () {
-            var choices = $("#add-new-choice").attr("data-variable").slice(1,-1).split("', ");
-            var str = "";
+        $(this).ready(function () {
+            let choices = $("#add-new-choice").attr("data-variable").slice(1, -1).split("', ");
+            let str = "";
 
             while (choices.length >= textBoxCounter-1) {
 
-                var oldTextBox = $("#id_choice_set-" + (textBoxCounter-1) + "-choice_text");
-                var newTextBox = oldTextBox.clone();
-                var choice = choices[textBoxCounter];
+                let oldTextBox = $("#id_choice_set-" + (textBoxCounter - 1) + "-choice_text");
+                let newTextBox = oldTextBox.clone();
+                let choice = choices[textBoxCounter];
                 str = choice;
 
-                if (textBoxCounter == choices.length-1) {
+                if (textBoxCounter === choices.length-1) {
                     str = choice.slice(1,-1);
                 }
                 else{
@@ -32,9 +32,9 @@ $(document).ready(function () {
                 newTextBox.attr("required", '');
                 newTextBox.val(str);
 
-                var newRowDiv = $("<div>").addClass('row').attr('id', "row_div_"+textBoxCounter).append(
-                                $("<div>").addClass('col-sm-2').text("Choice "+(textBoxCounter+1)),
-                                $("<div>").addClass('col-sm-6 form-group').attr('id', "col_div_"+textBoxCounter)
+                let newRowDiv = $("<div>").addClass('row').attr('id', "row_div_" + textBoxCounter).append(
+                    $("<div>").addClass('col-sm-2').text("Choice " + (textBoxCounter + 1)),
+                    $("<div>").addClass('col-sm-6 form-group').attr('id', "col_div_" + textBoxCounter)
                 );
 
                 $("#answer-bois").append(newRowDiv);
@@ -45,8 +45,8 @@ $(document).ready(function () {
     }
     // add more choices to a question
     $("#add-new-choice").click(function () {
-        var oldTextBox = $("#id_choice_set-" + (textBoxCounter-1) + "-choice_text");
-        var newTextBox = oldTextBox.clone();
+        let oldTextBox = $("#id_choice_set-" + (textBoxCounter - 1) + "-choice_text");
+        let newTextBox = oldTextBox.clone();
 
         newTextBox.attr("name",     "choice_set-"   + textBoxCounter + "-choice_text");
         newTextBox.attr("id",       "id_choice_set-"+ textBoxCounter + "-choice_text");
@@ -75,13 +75,13 @@ $(document).ready(function () {
 
     // adds default values as you add choices, this is on top of add-new-choice click
     $(".add-edit").click(function () {
-        var choices = $("#add-new-choice").attr("data-variable").slice(1,-1).split("', ");
-        var str = "";
+        let choices = $("#add-new-choice").attr("data-variable").slice(1, -1).split("', ");
+        let str = "";
 
         if (choices.length > textBoxCounter-1) {
-            var choice = choices[textBoxCounter-1];
+            let choice = choices[textBoxCounter - 1];
 
-            if (choices.length == textBoxCounter) {
+            if (choices.length === textBoxCounter) {
                 str = choice.slice(1,-1);
             }
             else {
@@ -105,10 +105,9 @@ $(document).ready(function () {
     // doing this instead of .post as I wanted to package the data in the form post data
     $(".select_me").select(function() {
             // grab the highlighted text on the select_me inputs
-        word_selection = $(this).selection();
-
-        parent_element = $($(this).parent());
-        choice_id = parent_element.attr('id');
+        let word_selection = $(this).selection();
+        let parent_element = $($(this).parent());
+        let choice_id = parent_element.attr('id');
 
         // removes the help text if we make a selection
         if ($('#annotation_help').length ) {
@@ -118,10 +117,10 @@ $(document).ready(function () {
         // if we have a caching field, just update val in it; else if we don't, create an input field and populate it
         if ($('#word_selection').length ) {
 
-            $('#word_selection').val(word_selection);
+            $(this).val(word_selection);
         }
         else {
-            var input = $("<input>").attr("name", "word_selection")
+            let input = $("<input>").attr("name", "word_selection")
                                     .attr("id", "word_selection")
                                     .attr("readonly", "")
                                     .attr("class", "textinput textInput form-control")
@@ -135,7 +134,7 @@ $(document).ready(function () {
             $('#choice_id_post').val(choice_id);
         }
         else {
-            var input = $("<input>").attr("name", "choice_id_selected")
+            let input = $("<input>").attr("name", "choice_id_selected")
                                     .attr("id", "choice_id_post")
                                     .attr("readonly", "")
                                     .attr("hidden", "")
@@ -143,12 +142,12 @@ $(document).ready(function () {
                                     .val(choice_id);
             $('#selection').append($(input));
         }
-
+        highlight($(this));
         updateAnnotationOperationHref();
     });
 
     $(".annotation_id").each(function() {
-        var input_field = $(($(this).children()[0]))
+        let input_field = $(($(this).children()[0]));
         console.log( input_field.val() );
     });
 
@@ -156,10 +155,10 @@ $(document).ready(function () {
 
     function updateAnnotationOperationHref() {
         $(".annotation_operation").each(function() {
-            var operation = $(this).attr('id');
-            var choice_id = $('#choice_id_post').val();
-            var word_text = $('#word_selection').val();
-            var class_name = $("#id_classification_name").val();
+            let operation = $(this).attr('id');
+            let choice_id = $('#choice_id_post').val();
+            let word_text = $('#word_selection').val();
+            let class_name = $("#id_classification_name").val();
 
             $(this).attr('href', './' + annotation_id + '/' + operation + '/' + choice_id + '/' + class_name + '/' + word_text);
             console.log($(this).attr('href'), choice_id, word_text, class_name);
@@ -177,7 +176,7 @@ $(document).ready(function () {
 // used in the results.html
 function create_chart(name, json_data) {
     FusionCharts.ready(function(){
-        var chart = new FusionCharts({
+        let chart = new FusionCharts({
             type: 'Column3D',
             width: '500',
             height: '300',
