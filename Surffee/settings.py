678318aae715f56dddf4afde6ab94a61665c00d0
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-06 19:12:41 +0100

    Mark exact issue with whtienoise

--- Surffee/settings.py
+++ Surffee/settings.py
@@ -144,9 +144,11 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# REMOVE THESE LINES SO LOCALHOST RUN ON PYCHARM WORKS
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-
+# HEROKU LINES
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+# REMOVE THIS LINE SO LOCALHOST RUN ON PYCHARM WORKS
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
