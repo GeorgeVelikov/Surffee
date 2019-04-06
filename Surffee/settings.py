eb1653343282f2f9969c44c652b0760f37e193e5
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-06 19:21:49 +0100

    Set up for build on heroku

--- Surffee/settings.py
+++ Surffee/settings.py
@@ -149,6 +149,6 @@ prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 # REMOVE THIS LINE SO LOCALHOST RUN ON PYCHARM WORKS
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
