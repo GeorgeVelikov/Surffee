b4dcef7e1e1362ef9c17ccbf7a6513f1b99baeb4
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-08 16:16:15 +0100

    Create basic link to single/multi term analysis pages

--- surveys/views/analysis.py
+++ surveys/views/analysis.py
@@ -41,3 +41,41 @@ class Create(CreateView):
             return redirect('/surveys/analysis/graph')
 
         return redirect('/surveys/analysis')
+
+
+class AnalysisSingleTerm(CreateView):
+    template_name = 'analysis/single.html'
+    model = Annotation
+    form_class = AnalysisCreator
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        all_surveys = Survey.objects.filter(creator=request.user).values('pk', 'name')
+
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  all_surveys=all_surveys,
+                                  )
+        )
+
+
+class AnalysisMultipleTerm(CreateView):
+    template_name = 'analysis/multi.html'
+    model = Annotation
+    form_class = AnalysisCreator
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        all_surveys = Survey.objects.filter(creator=request.user).values('pk', 'name')
+
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  all_surveys=all_surveys,
+                                  )
+        )
