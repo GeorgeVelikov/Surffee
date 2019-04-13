20055e2485e27485b06cbb112c0e5480a54a6a64
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-13 16:06:33 +0100

    Fix foreign key defaults

--- surveys/models/analysis.py
+++ surveys/models/analysis.py
@@ -8,8 +8,8 @@ from .survey import Survey
 class AnalysisSingle(models.Model):
     creator = models.ForeignKey(Researcher, on_delete=models.CASCADE, default=0)
     name = models.CharField(max_length=2**8)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
-    annotation = models.ForeignKey(Annotation, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, default=0)
+    annotation = models.ForeignKey(Annotation, on_delete=models.CASCADE, default=0)
 
     # this is the js "instance" variables
     terms = models.CharField(max_length=2**8)
@@ -19,6 +19,6 @@ class AnalysisSingle(models.Model):
 class AnalysisGraph(models.Model):
     creator = models.ForeignKey(Researcher, on_delete=models.CASCADE, default=0)
     name = models.CharField(max_length=2 ** 8)
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, default=0)
 
     questions_graphs = models.CharField(max_length=2**16)
