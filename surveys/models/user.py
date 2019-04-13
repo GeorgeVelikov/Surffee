cfed4bfbfb91475639983a610a5a07cdeeff8da3
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-13 18:34:11 +0100

    Make email unique

--- surveys/models/user.py
+++ surveys/models/user.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 class Researcher(AbstractUser):
     def __str__(self):
         return self.username
+
+    class Meta(object):
+        unique_together = ('email',)
