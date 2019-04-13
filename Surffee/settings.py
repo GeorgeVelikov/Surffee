adbad23d5ef0f97c72c4b2753b5e634f0d82b58b
Georgi V. Velikov <georgi.velikov.16@aberdeen.ac.uk>
2019-04-13 15:51:19 +0100

    Prepare for Heroku

--- Surffee/settings.py
+++ Surffee/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 # needed for heroku
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Custom backend stuff
@@ -150,4 +150,4 @@ prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
 # REMOVE THIS LINE SO LOCALHOST RUN ON PYCHARM WORKS
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
