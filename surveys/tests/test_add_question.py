48c63b75b3b84f5de955d61abf012eec6ad65a45
WoMart <m.woznicki.16@aberdeen.ac.uk>
2019-04-15 00:45:35 +0100

    test if moderate amount of choices can be added to a question

--- surveys/tests/test_add_question.py
+++ surveys/tests/test_add_question.py
@@ -9,9 +9,9 @@ from .static_test import create_survey
 
 
 # Test suite for adding question to an existing survey
-class CreateQuestionTests(TestCase):
+class AddQuestionTests(TestCase):
 
-    def add_question(self, q_text, t='S', choices=None):
+    def add_question(self, q_text, choices=None, t='S'):
         context = self.default_context
         context['question_text'] = q_text
         context['type'] = t
@@ -37,10 +37,10 @@ class CreateQuestionTests(TestCase):
             is_superuser=False
         )
         self.user.save()
+
         sur_name = 'Survey to test question addition etc.'
         create_survey(sur_name, self.user, False)
         self.sur = Survey.objects.get(name=sur_name)
-
         self.default_context = {
             'survey': self.sur.id,
             'q_text': 'default',
@@ -50,19 +50,17 @@ class CreateQuestionTests(TestCase):
             'choice_set-MAX_NUM_FORMS': 1000,
             'type': 'S',
         }
-        # TODO: test adding a question, question with no answers, question with no text
 
-    def test_create_question_status(self):
+    def test_add_question_status(self):
         """
         Check if the question adding page is being displayed
-        name of choice fields: choice_set-<id of field>-choice_text
         :return:
         """
         self.client.force_login(self.user)
         response = self.client.get(reverse('surveys:add_question', args=(self.sur.id,)))
         self.assertEqual(response.status_code, 200)
 
-    def test_create_question_no_question_text(self):
+    def test_add_question_no_question_text(self):
         """
          Check if user can create a question with no question text
         :return:
@@ -73,7 +71,7 @@ class CreateQuestionTests(TestCase):
         questions = [str(x) for x in self.sur.question_set.all()]
         self.assertNotIn(q_text, questions)
 
-    def test_create_question_no_answers(self):
+    def test_add_question_no_answers(self):
         """
          Check if user can create a question with no answers
         :return:
@@ -83,3 +81,26 @@ class CreateQuestionTests(TestCase):
         self.add_question(q_text)
         questions = [str(x) for x in self.sur.question_set.all()]
         self.assertIn(q_text, questions)
+
+    def test_add_question_with_ten_answers(self):
+        """
+         Check if question is added properly with moderate amount of answer choices provided
+        name of choice fields: choice_set-<id of field>-choice_text
+        :return:
+        """
+        self.client.force_login(self.user)
+        q_text = "This question contains 10 choices"
+        c_text = "Choice%d"
+        choices = {}
+        for x in range(10):
+            key = "choice_set-%d-choice_text" % x
+            choices[key] = c_text % x
+
+        self.add_question(q_text, choices)
+        questions = [str(x) for x in self.sur.question_set.all()]
+        self.assertIn(q_text, questions)
+        question = self.sur.question_set.get(question_text=q_text)
+        self.assertEqual(10, len(question.choice_set.all()), msg=str(question.choice_set.all()))
+        choice_texts = [str(x) for x in question.choice_set.all()]
+        for key in choices:
+            self.assertIn(choices[key], choice_texts)
